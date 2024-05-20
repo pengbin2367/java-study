dd826dab8ffed0940024ef18f8a6cf71d43f623f
PengBin <pengbin2367@163.com>
2024-05-20 15:11:49 +0800

    feat: JWT

--- 08-SSM/top-news/src/test/java/com/atqingke/topnews/TopNewsApplicationTests.java
+++ 08-SSM/top-news/src/test/java/com/atqingke/topnews/TopNewsApplicationTests.java
@@ -1,13 +1,26 @@
 package com.atqingke.topnews;
 
+import com.atqingke.topnews.util.JwtHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class TopNewsApplicationTests {
 
+	@Autowired
+	private JwtHelper jwtHelper;
+
 	@Test
 	void contextLoads() {
+		String token = jwtHelper.createToken(1L);
+		log.warn("token = {}", token);
+		Long userId = jwtHelper.getUserId(token);
+		log.warn("userId = {}", userId);
+		boolean expiration = jwtHelper.isExpiration(token);
+		log.warn("expiration = {}", expiration);
 	}
 
 }
