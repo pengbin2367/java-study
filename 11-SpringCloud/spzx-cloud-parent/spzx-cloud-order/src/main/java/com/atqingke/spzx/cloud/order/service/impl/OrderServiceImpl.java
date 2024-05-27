8762ed1a075370cb05a6a320794767dd35cd2c88
PengBin <pengbin2367@163.com>
2024-05-27 20:57:53 +0800

    feat: nacos register center

--- 11-SpringCloud/spzx-cloud-parent/spzx-cloud-order/src/main/java/com/atqingke/spzx/cloud/order/service/impl/OrderServiceImpl.java
+++ 11-SpringCloud/spzx-cloud-parent/spzx-cloud-order/src/main/java/com/atqingke/spzx/cloud/order/service/impl/OrderServiceImpl.java
@@ -5,8 +5,13 @@ import com.atqingke.spzx.cloud.order.entity.User;
 import com.atqingke.spzx.cloud.order.mapper.OrderMapper;
 import com.atqingke.spzx.cloud.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -17,14 +22,24 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    DiscoveryClient discoveryClient;
+
     @Override
     public Order findOrderByOrderId(Long orderId) {
         Order order = orderMapper.findOrderByOrderId(orderId);
 
-        User user = restTemplate.getForObject("http://localhost:10100/api/user/findUserByUserId/" + order.getUserId(), User.class);
+        ServiceInstance serviceInstance = findServiceInstance("spzx-cloud-user");
+
+        User user = restTemplate.getForObject("http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/api/user/findUserByUserId/" + order.getUserId(), User.class);
         order.setUser(user);
 
         return order;
     }
 
+    private ServiceInstance findServiceInstance(String serviceName) {
+        List<ServiceInstance> instances = discoveryClient.getInstances(serviceName);
+        Random random = new Random();
+        return instances.get(random.nextInt(instances.size()));
+    }
 }
