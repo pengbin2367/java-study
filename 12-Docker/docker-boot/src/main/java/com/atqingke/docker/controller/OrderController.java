2756722920f5d442937f580a1e597073130d7614
PengBin <pengbin2367@163.com>
2024-06-05 22:22:56 +0800

    fix: 浏览器无法访问6666端口

--- 12-Docker/docker-boot/src/main/java/com/atqingke/docker/controller/OrderController.java
+++ 12-Docker/docker-boot/src/main/java/com/atqingke/docker/controller/OrderController.java
@@ -2,22 +2,24 @@ package com.atqingke.docker.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@RequestMapping("order")
 @RestController
 public class OrderController {
 
     @Value("${server.port}")
     private String port;
 
-    @GetMapping("/order/docker")
+    @GetMapping("docker")
     public String helloDocker() {
         return "Hello Docker\t" + port + "\t" + UUID.randomUUID();
     }
 
-    @GetMapping("/order/index")
+    @GetMapping("index")
     public String index() {
         return "服务端口号\t" + port + "\t" + UUID.randomUUID();
     }
