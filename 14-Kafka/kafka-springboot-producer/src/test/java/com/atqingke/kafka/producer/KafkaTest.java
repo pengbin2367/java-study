236d53895db83476ef2051835bddc378e0a868e0
PengBin <pengbin2367@163.com>
2024-06-12 22:47:04 +0800

    feat: 数据有序

--- 14-Kafka/kafka-springboot-producer/src/test/java/com/atqingke/kafka/producer/KafkaTest.java
+++ 14-Kafka/kafka-springboot-producer/src/test/java/com/atqingke/kafka/producer/KafkaTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import java.util.concurrent.ExecutionException;
+
 @SpringBootTest
 public class KafkaTest {
 
@@ -73,5 +75,30 @@ public class KafkaTest {
         kafkaTemplate.send(topicName, "zhu zhu atqingke zhu");
         kafkaTemplate.send(topicName, "zhu zhu pengbin zhu");
         kafkaTemplate.send(topicName, "zhu zhu happy zhu");
+    }
+
+    @Test
+    public void testSendMessageOrder() throws ExecutionException, InterruptedException {
+        String topicName = "topic-spring-boot";
+        // 通过 partition 限定消息发送到同一个分区
+        Integer partition = 0;
+        String key = null;
+        // 为什么要调用 get() 方法？
+        // 并不是为了获取任务执行的结果
+        // 而是确保第一个任务执行完成之后，再执行第二个任务
+        // 如果不这样做，各个子线程负责发送消息没办法保证发送消息的顺序
+        kafkaTemplate.send(topicName, partition, key, new UserDTO("order01", 10, "1111")).get();
+        kafkaTemplate.send(topicName, partition, key, new UserDTO("order02", 20, "2222")).get();
+        kafkaTemplate.send(topicName, partition, key, new UserDTO("order03", 30, "3333")).get();
+    }
+
+    @Test
+    public void testSendMessageNoOrder() throws ExecutionException, InterruptedException {
+        String topicName = "topic-spring-boot";
+
+        for (int i = 0; i < 18; i++) {
+            // 每条消息发送到不同分区，看看消费端接收消息的顺序
+            kafkaTemplate.send(topicName, i % 3, null, new UserDTO("order" + i, i * 10, i * 100 + "")).get();
+        }
     }
 }
