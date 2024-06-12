b91007663476fde32cda376e70c58d4f973fccc6
PengBin <pengbin2367@163.com>
2024-06-12 19:10:20 +0800

    fix: 发送非字符串消息

--- 14-Kafka/kafka-springboot-producer/src/test/java/com/atqingke/kafka/producer/KafkaTest.java
+++ 14-Kafka/kafka-springboot-producer/src/test/java/com/atqingke/kafka/producer/KafkaTest.java
@@ -1,5 +1,6 @@
 package com.atqingke.kafka.producer;
 
+import com.atqingke.kafka.producer.entity.UserDTO;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,13 @@ public class KafkaTest {
 
     @Test
     void testSendMessage() {
-        kafkaTemplate.send("topic-spring-boot", "hello spring boot message");
+        kafkaTemplate.send("topic-spring-boot", "hello spring boot message1111");
+    }
+
+    @Test
+    public void testSendEntity() {
+        String topicName = "topic-spring-boot";
+        UserDTO userDTO = new UserDTO("tom", 25, "12345343");
+        kafkaTemplate.send(topicName, userDTO);
     }
 }
