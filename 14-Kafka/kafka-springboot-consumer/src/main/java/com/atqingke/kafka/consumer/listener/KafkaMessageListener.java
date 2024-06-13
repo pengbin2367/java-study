a284c712c3ce163384aeff68ec7075dc1c892eb3
PengBin <pengbin2367@163.com>
2024-06-13 10:35:16 +0800

    feat: 消费端指定offset

--- 14-Kafka/kafka-springboot-consumer/src/main/java/com/atqingke/kafka/consumer/listener/KafkaMessageListener.java
+++ 14-Kafka/kafka-springboot-consumer/src/main/java/com/atqingke/kafka/consumer/listener/KafkaMessageListener.java
@@ -2,7 +2,8 @@ package com.atqingke.kafka.consumer.listener;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.annotation.PartitionOffset;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
@@ -16,22 +17,39 @@ public class KafkaMessageListener {
 //        acknowledgment.acknowledge();
 //    }
 
-    @RetryableTopic
-    @KafkaListener(topics = {"topic-spring-boot"})
+//    @RetryableTopic
+//    @KafkaListener(topics = {"topic-spring-boot"})
+//    public void simpleConsumerPartition(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
+//        System.out.println(record.partition() + " " + record.offset() + " " + record.value());
+//
+//        System.out.println(10 / 0);
+//
+//        // 消息处理完成后，手动提交
+//        acknowledgment.acknowledge();
+//    }
+//
+//    // 死信主题会自动在原主题名称后附加“-dlt”
+//    @KafkaListener(topics = {"topic-spring-boot-dlt"})
+//    public void deadLetterProcess(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
+//
+//        System.out.println("死信主题：" + record.partition() + " " + record.offset() + " " + record.value());
+//
+//        // 消息处理完成后，手动提交
+//        acknowledgment.acknowledge();
+//    }
+
+    @KafkaListener(topicPartitions = {
+            @TopicPartition(
+                    topic = "topic-spring-boot",
+                    partitionOffsets = {
+                            @PartitionOffset(partition = "0", initialOffset = "0"),
+                            @PartitionOffset(partition = "1", initialOffset = "50"),
+                            @PartitionOffset(partition = "2", initialOffset = "0", relativeToCurrent = "true")
+                    }
+            )
+    })
     public void simpleConsumerPartition(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
         System.out.println(record.partition() + " " + record.offset() + " " + record.value());
-
-        System.out.println(10 / 0);
-
-        // 消息处理完成后，手动提交
-        acknowledgment.acknowledge();
-    }
-
-    // 死信主题会自动在原主题名称后附加“-dlt”
-    @KafkaListener(topics = {"topic-spring-boot-dlt"})
-    public void deadLetterProcess(ConsumerRecord<String, String> record, Acknowledgment acknowledgment) {
-
-        System.out.println("死信主题：" + record.partition() + " " + record.offset() + " " + record.value());
 
         // 消息处理完成后，手动提交
         acknowledgment.acknowledge();
