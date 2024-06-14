5415cc00e81bbdf4604bce1e5ada6ef3bdb94b78
PengBin <pengbin2367@163.com>
2024-06-14 17:53:23 +0800

    feat: security demo

---
+++ 15-SpringSecurity/security-demo/init.sql
@@ -0,0 +1,20 @@
+-- 创建数据库
+CREATE DATABASE `security-demo`;
+USE `security-demo`;
+
+-- 创建用户表
+CREATE TABLE `user`
+(
+    `id`       INT     NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(50)  DEFAULT NULL,
+    `password` VARCHAR(500) DEFAULT NULL,
+    `enabled`  BOOLEAN NOT NULL
+);
+-- 唯一索引
+CREATE UNIQUE INDEX `user_username_uindex` ON `user` (`username`);
+
+-- 插入用户数据(密码是 "abc" )
+INSERT INTO `user` (`username`, `password`, `enabled`)
+VALUES ('admin', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', TRUE),
+       ('Helen', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', TRUE),
+       ('Tom', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', TRUE);
