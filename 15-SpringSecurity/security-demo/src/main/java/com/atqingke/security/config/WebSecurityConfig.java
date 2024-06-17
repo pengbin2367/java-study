bdee1b53fb9f75fe42775b7f11e3100cf317b11b
PengBin <pengbin2367@163.com>
2024-06-17 15:08:08 +0800

    feat: 基于方法的授权

--- 15-SpringSecurity/security-demo/src/main/java/com/atqingke/security/config/WebSecurityConfig.java
+++ 15-SpringSecurity/security-demo/src/main/java/com/atqingke/security/config/WebSecurityConfig.java
@@ -3,12 +3,14 @@ package com.atqingke.security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 @EnableWebSecurity//Spring项目总需要添加此注解，SpringBoot项目中不需要
 public class WebSecurityConfig {
 
--- 15-SpringSecurity/security-demo/src/main/java/com/atqingke/security/controller/UserController.java
+++ 15-SpringSecurity/security-demo/src/main/java/com/atqingke/security/controller/UserController.java
@@ -3,6 +3,7 @@ package com.atqingke.security.controller;
 import com.atqingke.security.entity.User;
 import com.atqingke.security.service.UserService;
 import jakarta.annotation.Resource;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +15,13 @@ public class UserController {
     @Resource
     public UserService userService;
 
+    @PreAuthorize("hasRole('ADMIN') and authentication.name == 'admim'")
     @GetMapping("/list")
     public List<User> getList(){
         return userService.list();
     }
 
+    @PreAuthorize("hasAuthority('USER_ADD')")
     @PostMapping("/add")
     public void add(@RequestBody User user){
         userService.saveUserDetails(user);
